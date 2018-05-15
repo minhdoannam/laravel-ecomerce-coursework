71cd3deb22f949aa73950862f58a8ded5ea1255b
Minh DN <namminhlp@gmail.com>
2018-05-15 22:07:56 +0700

    laravel setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>4 Man - Shirt</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/custom.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    a {
+        color: black;
+    }
+
+    a:hover {
+        color: #544949;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">4 Man - Shirt</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <div id="imaginary_container">
+                            <div class="input-group stylish-input-group">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-addon">
+                        <button type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>  
+                    </span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Áo thun không cổ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Áo thun có cổ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Áo hoodie</a>
+                    </li>
+                    <li>
+                        <span class="navRbcart">
+              <i class=" fa fa-shopping-cart shopping_bg" aria-hidden="true"></i>
+            </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('storage/img/bg3.jpg')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>First Slide</h3>
+                        <p>This is a description for the first slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('storage/img/bg2.jpg')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Second Slide</h3>
+                        <p>This is a description for the second slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('storage/img/bg1.jpg')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Third Slide</h3>
+                        <p>This is a description for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+        </div>
+    </header>
+    <!-- Page Content -->
+    <div class="container">
+        <h1 class="my-4">Recommendations For You</h1>
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+            <img src="/img/ts1.jpg" class="imgProduct">
+            <strong>Renewable Energy tshirt</strong>
+            <p>by Weenietees</p>
+          </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+            <img src="/img/ts5.jpg" class="imgProduct">
+            <strong>Renewable Energy tshirt</strong>
+            <p>by Weenietees</p>
+          </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+          <img src="/img/ts3.jpg" class="imgProduct">
+          <strong>Renewable Energy tshirt</strong>
+          <p>by Weenietees</p>
+        </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+                        <div style="height: 255px">
+                            <img src="/img/ts4.jpg" class="imgProduct">
+                        </div>
+                        <div>
+                            <strong>Renewable Energy tshirt</strong>
+                            <p>by Weenietees</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Portfolio Section -->
+        <div class="row spaceProduct">
+            <div class="col-lg-3 dvTitlepd">
+                <h3>Áo thun có cổ</h3>
+            </div>
+            <div class="col-md-9 dvTitlehr"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="/img/ts1.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="/img/ts5.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+      <img src="/img/ts3.jpg" class="imgProduct">
+      <strong>Renewable Energy tshirt</strong>
+      <p>by Weenietees</p>
+    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+                        <div style="height: 255px">
+                            <img src="storage/img/ts4.jpg" class="imgProduct">
+                        </div>
+                        <div>
+                            <strong>Renewable Energy tshirt</strong>
+                            <p>by Weenietees</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row spaceProduct">
+            <div class="col-lg-3 dvTitlepd">
+                <h3>Áo hoodie</h3>
+            </div>
+            <div class="col-md-9 dvTitlehr"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="img/ts1.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="img/ts5.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+      <img src="img/ts3.jpg" class="imgProduct">
+      <strong>Renewable Energy tshirt</strong>
+      <p>by Weenietees</p>
+    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+                        <div style="height: 255px">
+                            <img src="img/ts4.jpg" class="imgProduct">
+                        </div>
+                        <div>
+                            <strong>Renewable Energy tshirt</strong>
+                            <p>by Weenietees</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row spaceProduct">
+            <div class="col-lg-3 dvTitlepd">
+                <h3>Áo thun không cổ</h3>
+            </div>
+            <div class="col-md-9 dvTitlehr"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="img/ts1.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+        <img src="img/ts5.jpg" class="imgProduct">
+        <strong>Renewable Energy tshirt</strong>
+        <p>by Weenietees</p>
+      </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+      <img src="img/ts3.jpg" class="imgProduct">
+      <strong>Renewable Energy tshirt</strong>
+      <p>by Weenietees</p>
+    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dvproduct">
+                    <a href="">
+                        <div style="height: 255px">
+                            <img src="img/ts4.jpg" class="imgProduct">
+                        </div>
+                        <div>
+                            <strong>Renewable Energy tshirt</strong>
+                            <p>by Weenietees</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Modern Business Features</h2>
+                <p>The Modern Business template by Start Bootstrap includes:</p>
+                <ul>
+                    <li>
+                        <strong>Bootstrap v4</strong>
+                    </li>
+                    <li>jQuery</li>
+                    <li>Font Awesome</li>
+                    <li>Working contact form with validation</li>
+                    <li>Unstyled page elements for easy customization</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="http://placehold.it/700x450" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+        <hr>
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="#">Call to Action</a>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
