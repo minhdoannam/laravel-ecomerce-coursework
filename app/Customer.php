270a9f0f4ad7895f3af4ce3e6ccef2800a9f2bc7
Minh DN <namminhlp@gmail.com>
2018-06-22 12:48:28 +0700

    final_update, not fix bug yet

--- app/Customer.php
+++ app/Customer.php
@@ -10,5 +10,11 @@ class Customer extends Model
     protected $table = 'customers';
 	public $timestamp = true;
 
-	
+	protected $hidden = [
+        'password', 'rememberToken',
+    ];
+
+    protected $fillable = [
+        'firstName', 'lastName','dateRegister', 'email', 'password', 'phone', 'address', 'address2', 'city', 'country']
+        
 }
