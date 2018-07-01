d003d5e93a98bc578dacc63d1b8336edc39ba4c8
Minh DN <namminhlp@gmail.com>
2018-07-01 09:39:30 +0700

    done Cart

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,11 +1,6 @@
 <footer class="py-5 bg-dark text-muted text-center text-small">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
-             <ul class="list-inline text-muted text-light">
-          <li class="list-inline-item"><a href="#">Privacy</a></li>
-          <li class="list-inline-item"><a href="#">Terms</a></li>
-          <li class="list-inline-item"><a href="#">Support</a></li>
-        </ul>
         </div>
         <!-- /.container -->
 </footer>
