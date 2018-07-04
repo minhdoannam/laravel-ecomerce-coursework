ad78017e09f6ea641480584e9919ae2465ea539c
Minh DN <namminhlp@gmail.com>
2018-07-04 18:53:56 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'http://db4free.net'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel_ecomerce'),
             'username' => env('DB_USERNAME', 'minhdn'),
